d70516eaadfef84f0d94b40042f6f9bfed7a4c4e
mael.zpt <mael.mzg@gmail.com>
2026-04-02 15:27:55 +0200

    correction minime

--- src/config.py
+++ src/config.py
@@ -7,5 +7,3 @@ PROJECT_ROOT = Path(__file__).resolve().parent.parent
 DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 POKER_DATA_DIR = RAW_DATA_DIR / "poker_nlh"
-
-KAGGLE_DATASET_HANDLE = "smeilz/poker-holdem-games"
